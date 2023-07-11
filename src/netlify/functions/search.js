538f2a075768bd5b6da21ee1395bfe8acd72fdca
Arushi <arushi.a052@gmail.com>
2023-07-11 14:41:52 -0700

    netlify function

---
+++ src/netlify/functions/search.js
@@ -0,0 +1,20 @@
+const fetch = require('node-fetch');
+
+exports.handler = async (event, context) => {
+  try {
+    const keyword = event.queryStringParameters.query;
+    const resp = await fetch('https://testtechnext1-pearl118.b4a.run/search/api/query/?query=' + keyword);
+    console.log(await resp.text());
+    const jsonResp = await resp.json();
+    console.log(jsonResp);
+    return {
+      statusCode: 200,
+      body: JSON.stringify(jsonResp),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Could not fetch data' }),
+    };
+  }
+};
